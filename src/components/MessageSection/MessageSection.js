b3fbcaaeb72a7cd91a12e5694ca78ff03ded6462
My <my.j97@protonmail.com>
2023-11-02 17:32:29 +0100

    form

--- src/components/MessageSection/MessageSection.js
+++ src/components/MessageSection/MessageSection.js
@@ -9,10 +9,12 @@ const MessageSection = () => {
           <h2>Leave us a message<br/> for any information.</h2>
         </div>
         <div className="search-container">
-          <input type="text" id="search-input" placeholder="Name*" />
-          <input type="text" id="search-input" placeholder="Email*" />
-          <input type="text" id="search-input-message" placeholder="Your Message*" />
-          <a className="btn-yellow" href="Subscribe.html">Send Message <i className="fa-regular fa-arrow-up-right"></i></a>
+          <form>
+            <input type="text" id="search-input" placeholder="Name*" />
+            <input type="text" id="search-input" placeholder="Email*" />
+            <input type="text" id="search-input-message" placeholder="Your Message*" />
+            <a className="btn-yellow" href="Subscribe.html">Send Message <i className="fa-regular fa-arrow-up-right"></i></a>
+          </form>
         </div>
       </div>
     </div>

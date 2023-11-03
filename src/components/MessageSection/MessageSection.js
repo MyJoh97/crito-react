fbc450274e4c69f08935329b1a8685203bec64fe
My <my.j97@protonmail.com>
2023-11-03 15:09:17 +0100

    validate forms

--- src/components/MessageSection/MessageSection.js
+++ src/components/MessageSection/MessageSection.js
@@ -5,9 +5,20 @@ import { useState } from 'react'
 
 const MessageSection = () => {
   const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [error, setError] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    console.log(email + ' | ' + message)
+
+    if (!message) {
+      setError(<p className="errorMessage">Message is empty. Please type a message.</p>)
+    } else {
+      setError('')
+    }
   }
   return (
     <div className="leave-message-information">
@@ -19,8 +30,11 @@ const MessageSection = () => {
           <div className="search-container">
             <form>
               <input type="text" id="search-input-name" placeholder="Name*" value={name} onChange={ (e) => setName(e.target.value)} />
-              <input type="text" id="search-input-email" placeholder="Email*" />
-              <input type="text" id="search-input-message" placeholder="Your Message*" />
+              <input type="text" id="search-input-email" placeholder="Email*" value={email} onChange={ (e) => setEmail(e.target.value)} />
+              <input type="text" id="search-input-message" placeholder="Your Message*" value={message} onChange={ (e) => setMessage(e.target.value)} />
+              
+              {error}
+
               <a className="btn-yellow" href="Subscribe.html" type="submit" onClick={handleSubmit}>Send Message <i className="fa-regular fa-arrow-up-right"></i></a>
             </form>
           </div>

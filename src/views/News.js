f04df80cf7efdc77b2a4a53b02a946f6e4acf3bf
My <my.j97@protonmail.com>
2023-11-16 01:34:19 +0100

    Api

--- src/views/News.js
+++ src/views/News.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Header from '../components/Header';
 import NewsBody from '../components/NewsBody'
 import Footer from '../components/Footer';
-
+import NewsSignup from '../components/NewsSignup/NewsSignup'
 
 const News = () => {
   return (
     <>
     <Header />
     <NewsBody />
+    <NewsSignup />
     <Footer />
     </>
   )
